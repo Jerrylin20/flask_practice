afbc7d84ca39975ac3a70a6f0232b5d23dda682d
Jerrylin20 <1211003lin@gmail.com>
2023-02-16 00:49:45 +0900

    add substract

--- tests/src/test_calculator.py
+++ tests/src/test_calculator.py
@@ -5,3 +5,7 @@ cal = Calculator()
 def test_add():
     assert cal.add(1,1) == 2
     assert cal.add(2,2) == 4
+
+def test_substract():
+    assert cal.substract(2,1) == 1
+    assert cal.substract(10,2 == 8
